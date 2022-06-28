e2f97979d25153228ce0ef179d876b15ca6d60f9
tahmimhridoy <thamimhasanhridoyea@gmail.com>
2022-06-28 21:24:25 +0600

    service

--- js/app.js
+++ js/app.js
@@ -18,4 +18,20 @@ $(document).ready(function(){
         startDelay: 1000
     });
 
+    // progress bar
+    var waypoint = new Waypoint({
+        element: document.getElementById('exp-id'),
+        handler: function() {
+            var p = document.querySelectorAll(".progress-bar");
+            p[0].setAttribute("style", "width: 100%; transition: .5s all;")
+            p[1].setAttribute("style", "width: 95%; transition: 1.5s all;")
+            p[2].setAttribute("style", "width: 85%; transition: 1.7s all;")
+            p[3].setAttribute("style", "width: 99%; transition: 2s all;")
+            p[4].setAttribute("style", "width: 85%; transition: 2.3s all;")
+        },
+        offset: '90%',
+    })
+
+    
+
 });
